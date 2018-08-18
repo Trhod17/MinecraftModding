494435555b4a8472e8082c5d395dacca6d81d1aa
HarryTechRevs <30769752+HarryTechRevs@users.noreply.github.com>
2018-08-18 18:29:00 +0100

    Add files via upload

--- main/java/harry/mod/init/BlockInit.java
+++ main/java/harry/mod/init/BlockInit.java
@@ -1,17 +1,14 @@
-package harry.mod.init;
+package harry.mods.tutorialmod.init;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import harry.mod.objects.blocks.BlockBase;
-import harry.mod.objects.blocks.BlockDirts;
-import harry.mod.objects.blocks.BlockLeaf;
-import harry.mod.objects.blocks.BlockLogs;
-import harry.mod.objects.blocks.BlockOres;
-import harry.mod.objects.blocks.BlockPlank;
-import harry.mod.objects.blocks.BlockSantaHat;
-import harry.mod.objects.blocks.BlockSaplings;
-import harry.mod.objects.blocks.machines.sinterer.BlockSinteringFurnace;
+import harry.mods.tutorialmod.Main;
+import harry.mods.tutorialmod.blocks.BlockBase;
+import harry.mods.tutorialmod.blocks.BlockCopperChest;
+import harry.mods.tutorialmod.blocks.BlockElectricSinteringFurnace;
+import harry.mods.tutorialmod.blocks.BlockGlowstoneGenerator;
+import harry.mods.tutorialmod.blocks.BlockSinteringFurnace;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 
@@ -19,20 +16,10 @@ public class BlockInit
 {
 	public static final List<Block> BLOCKS = new ArrayList<Block>();
 	
-	public static final Block BLOCK_COPPER = new BlockBase("block_copper", Material.IRON);
-	
-	public static final Block ORE_END = new BlockOres("ore_end", "end");
-	public static final Block ORE_OVERWORLD = new BlockOres("ore_overworld", "overworld");
-	public static final Block ORE_NETHER = new BlockOres("ore_nether", "nether");
-	
-	public static final Block PLANKS = new BlockPlank("planks");
-	public static final Block LOGS = new BlockLogs("log");
-	public static final Block LEAVES = new BlockLeaf("leaves");
-	public static final Block SAPLINGS = new BlockSaplings("sapling");	
-	public static final Block DIRT = new BlockDirts("dirt");
-	
-	public static final Block SANTA_HAT = new BlockSantaHat("santa_hat");
+	public static final Block COPPER_BLOCK = new BlockBase("copper_block", Material.IRON, Main.TUTORIAL);
+	public static final Block COPPER_CHEST = new BlockCopperChest("copper_chest");
 	
 	public static final Block SINTERING_FURNACE = new BlockSinteringFurnace("sintering_furnace");
-
+	public static final Block ELECTRIC_SINTERING_FURNACE = new BlockElectricSinteringFurnace("electric_sintering_furnace");
+	public static final Block GLOWSTONE_GENERATOR = new BlockGlowstoneGenerator("glowstone_generator");
 }
